3e4600884ceaaad5869bf641cdf46fe0d107bceb
cxn <1353337123@qq.com>
2017-12-13 18:09:45 +0800

    分支提交1

--- src/main/java/com/lzy/seek/SeekApplication.java
+++ src/main/java/com/lzy/seek/SeekApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 
- * @author 李兆阳
+ * @author 陈喜楠
  * @description : [seek启动入口]
  *
  * @时间: 2017年10月19日 上午10:59:36
@@ -14,6 +14,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.lzy.seek.dao")
 public class SeekApplication {
 
+	/**
+	 * 
+	 * @TODO: [run as main方法就能执行]
+	 * @param args: 
+	 * @createTime:2017年12月13日下午6:07:46
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SeekApplication.class, args);
 	}
